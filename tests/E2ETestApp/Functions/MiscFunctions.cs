cec1f214ea79f19d7627b47a5eb557dfa9f06315
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-07-28 14:35:04 -0400

    add example from readme

---
+++ tests/E2ETestApp/Functions/MiscFunctions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Tizzani.AzureFunctionsWorkerHttpExtensions;
+
+namespace E2ETestApp.Functions;
+
+public sealed class MiscFunctions
+{
+    [Function(nameof(PocoAndPrimitiveCombo))]
+    public async Task<HttpResponseData> PocoAndPrimitiveCombo(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [BindQuery] Person? person,
+        [BindQuery] string? description)
+    {
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+
+        await response.WriteAsJsonAsync(new 
+        {
+            person?.Name,
+            person?.Age,
+            description 
+        });
+
+        return response;
+    }
+
+    public record Person(string Name, int Age);
+}
