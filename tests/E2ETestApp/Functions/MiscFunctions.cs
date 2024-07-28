51bdf6385d6a398ecdcf3d4e177a40e0c67bfb13
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-07-28 15:47:21 -0400

    removed unused test functions

--- tests/E2ETestApp/Functions/MiscFunctions.cs
+++ tests/E2ETestApp/Functions/MiscFunctions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using Azure;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Tizzani.AzureFunctionsWorkerHttpExtensions;
 
@@ -24,5 +25,41 @@ public sealed class MiscFunctions
         return response;
     }
 
+    [Function(nameof(MiscFunction1))]
+    public async Task<HttpResponseData> MiscFunction1(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [BindQuery] FilterDto filter)
+    {
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(filter);
+        return response;
+    }
+
+    [Function(nameof(MiscFunction2))]
+    public async Task<HttpResponseData> MiscFunction2(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [BindQuery] string id,
+        [BindQuery] int skip,
+        [BindQuery] int? take) // TODO: add support for default values (e.g., take = 10)
+    {
+        var filter = new FilterDto
+        {
+            Id = id,
+            Skip = skip,
+            Take = take ?? 10
+        };
+
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(filter);
+        return response;
+    }
+
     public record Person(string Name, int Age);
+
+    public class FilterDto
+    {
+        public string Id { get; set; } = "";
+        public int Skip { get; set; }
+        public int Take { get; set; } = 10;
+    }
 }
